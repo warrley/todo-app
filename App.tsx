21484205380f474758b63083de70c762b6f5c4c2
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-10 16:46:48 -0300

    header done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, StatusBar, View, Text, TextInput, Pressable } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={s.container}>
+      <View style={s.header}>
+        <Text style={s.title}>Tasks</Text>
+        <View style={{ flexDirection: "row", gap: 10 }}>
+          <TextInput
+            style={s.input}
+            placeholder="Enter your new task..."
+          />
+          <Pressable style={s.button}>
+            <FontAwesome name="plus" color="white" size={20}/>
+          </Pressable>
+        </View>
+      </View>
+      <View>
+        <View>
+          
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "white",
+    flex: 1
   },
+  header: {
+    backgroundColor: "#22272e",
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    paddingBottom: 30
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 10
+  },
+  input: {
+    backgroundColor: "white",
+    flex: 1,
+    borderRadius: 7,
+    fontSize: 20,
+    paddingHorizontal: 10
+  },
+  button: {
+    backgroundColor: "aqua",
+    borderRadius: 7,
+    height: 50,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
